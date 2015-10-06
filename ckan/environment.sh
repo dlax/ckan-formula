8c5512280b00ad63829490ba199d7b1503b9f275
Denis Laxalde <denis.laxalde@logilab.fr>
2015-10-06 17:09:03 +0200

    Set CKAN_SITE_URL env variable

--- ckan/environment.sh
+++ ckan/environment.sh
@@ -2,3 +2,4 @@
 export CKAN_SQLALCHEMY_URL=postgresql://{{ ckan.db_user }}:{{ ckan.db_password }}@{{ ckan.db_host }}/{{ ckan.db_name }}
 export CKAN_DATASTORE_WRITE_URL=postgresql://{{ ckan.db_user }}:{{ ckan.db_password }}@{{ ckan.db_host }}/{{ ckan.datastore_db_name }}
 export CKAN_DATASTORE_READ_URL=postgresql://{{ ckan.datastore_db_user }}:{{ ckan.datastore_db_password }}@{{ ckan.db_host }}/{{ ckan.datastore_db_name }}
+export CKAN_SITE_URL={{ ckan.site_url }}
