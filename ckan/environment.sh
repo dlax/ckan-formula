3bf92c0bd1be89db8362f6cf4aca2aadd292a811
Denis Laxalde <denis.laxalde@logilab.fr>
2015-10-06 17:17:56 +0200

    Set solr_url env variable

--- ckan/environment.sh
+++ ckan/environment.sh
@@ -3,3 +3,4 @@ export CKAN_SQLALCHEMY_URL=postgresql://{{ ckan.db_user }}:{{ ckan.db_password }
 export CKAN_DATASTORE_WRITE_URL=postgresql://{{ ckan.db_user }}:{{ ckan.db_password }}@{{ ckan.db_host }}/{{ ckan.datastore_db_name }}
 export CKAN_DATASTORE_READ_URL=postgresql://{{ ckan.datastore_db_user }}:{{ ckan.datastore_db_password }}@{{ ckan.db_host }}/{{ ckan.datastore_db_name }}
 export CKAN_SITE_URL={{ ckan.site_url }}
+export CKAN_SOLR_URL=http://localhost:8983/solr
