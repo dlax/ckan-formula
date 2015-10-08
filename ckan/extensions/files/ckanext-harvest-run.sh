c4cf793dadea711fd0c602bb56268d534f77ef55
Denis Laxalde <denis.laxalde@logilab.fr>
2015-10-08 14:37:23 +0200

    Set PYTHONPATH in ckanext-harvest-run.sh

--- ckan/extensions/files/ckanext-harvest-run.sh
+++ ckan/extensions/files/ckanext-harvest-run.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 {% from "ckan/map.jinja" import ckan with context %}
+export PYTHONPATH={{ [ckan.src_dir, 'ckan']|join('/') }}:$PYTHONPATH
 {{ ckan.venv_path }}/bin/paster --plugin=ckanext-harvest harvester run --config={{ [ckan.confdir, ckan.conffile]|join('/') }}
