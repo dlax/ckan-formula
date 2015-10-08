1cc2e5b849d7310095c784634479ddc9929ee052
Denis Laxalde <denis.laxalde@logilab.fr>
2015-10-08 09:35:22 +0200

    Make ckanext-harvest-run.sh working

--- ckan/extensions/files/ckanext-harvest-run.sh
+++ ckan/extensions/files/ckanext-harvest-run.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 {% from "ckan/map.jinja" import ckan with context %}
-source {{ [ckan.confdir, 'environment.sh']|join('/') }}
-{{ ckan.ckan_home }}/bin/paster --plugin=ckanext-harvest harvester run --config={{ [ckan.confdir, ckan.conffile]|join('/') }}
+. {{ [ckan.confdir, 'environment.sh']|join('/') }}
+{{ ckan.venv_path }}/bin/paster --plugin=ckanext-harvest harvester run --config={{ [ckan.confdir, ckan.conffile]|join('/') }}
