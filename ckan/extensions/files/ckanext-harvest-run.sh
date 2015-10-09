27261bc942f5ab96bbb11fbfe406f17b69057e2d
Denis Laxalde <denis.laxalde@logilab.fr>
2015-10-09 17:03:25 +0200

    Redirect output of harvester run script into a log file

--- ckan/extensions/files/ckanext-harvest-run.sh
+++ ckan/extensions/files/ckanext-harvest-run.sh
@@ -1,4 +1,7 @@
 #/bin/sh
 {% from "ckan/map.jinja" import ckan with context %}
 export PYTHONPATH={{ [ckan.src_dir, 'ckan']|join('/') }}:$PYTHONPATH
-{{ ckan.venv_path }}/bin/paster --plugin=ckanext-harvest harvester run --config={{ [ckan.confdir, ckan.conffile]|join('/') }}
+{{ ckan.venv_path }}/bin/paster \
+  --plugin=ckanext-harvest harvester run \
+  --config={{ [ckan.confdir, ckan.conffile]|join('/') }} \
+  >> {{ [ckan.ckan_home, 'harvester.log']|join('/') }} 2>&1
